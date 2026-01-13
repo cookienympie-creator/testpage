59ea5676aee987c66988b3a474c4ebd147bfbff1
cookienympie-creator <cookienympie@gmail.com>
2026-01-13 21:22:05 +0100

    chore: remove .next from tracking and add gitignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/onyix/Documents/Msnip/test-webpage/test-webpage/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
